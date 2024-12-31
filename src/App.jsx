f59420052995cd8e1dc0d7ceb00dbfe1d055914b
Bemnet <bemnetkibret4@gmail.com>
2024-12-31 18:38:25 +0300

    done with responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
 import QuestionPage from './Components/QuestionPage/QuestionPage';
 import QuestionDetail from './Components/QuestionDetail/QuestionDetail';
-import AskQuestion from './Components/AskQuestion/AskQuestion'; 
+import AskQuestion from './Components/AskQuestion/AskQuestion';
 import ProtectedRoute from './Components/ProtectedRoute/ProtectedRoute';
-import HowItWorks from './Components/HowItWorks/HowItWorks'
+import HowItWorks from './Components/HowItWorks/HowItWorks';
 import { AuthProvider } from './Hooks/AuthContext';
 import './App.css';
 
 function App() {
   return (
     <AuthProvider>
+      <Router>
         <Header />
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<ProtectedRoute><QuestionPage /></ProtectedRoute>} />
-          <Route path='/howitworks' element={<HowItWorks/>} />
+          <Route path='/howitworks' element={<HowItWorks />} />
           <Route path="/questions/:question_id" element={<ProtectedRoute><QuestionDetail /></ProtectedRoute>} />
           <Route path="/ask" element={<ProtectedRoute><AskQuestion /></ProtectedRoute>} />
         </Routes>
         <Footer />
+      </Router>
     </AuthProvider>
   );
 }
